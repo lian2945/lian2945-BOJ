8b050cfb078a3fba2167da6e755496989872b6c1
lian2945 <167195801+lian2945@users.noreply.github.com>
2024-07-03 21:57:43 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void)
+{
+    priority_queue<int, vector<int>, greater<int>> arr;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, idx;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        cin >> idx;
+        if(idx==0)
+        {
+            if(arr.empty())
+            {
+                cout << 0 << "\n";
+            }
+            else
+            {
+                cout << arr.top() << "\n";
+                arr.pop();
+            }
+        }
+        else
+        {
+            arr.push(idx);
+        }
+    }
+    return 0;
+}
