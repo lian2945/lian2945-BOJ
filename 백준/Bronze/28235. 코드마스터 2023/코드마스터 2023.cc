61d63be5a82753ca806b1af1d584c14d437386c7
lian2945 <167195801+lian2945@users.noreply.github.com>
2024-07-12 11:40:45 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    string str;
+    cin >> str;
+    if(str=="SONGDO") cout << "HIGHSCHOOL";
+    if(str=="CODE") cout << "MASTER";
+    if(str=="2023") cout << "0611";
+    if(str=="ALGORITHM") cout << "CONTEST";
+    return 0;
+}
