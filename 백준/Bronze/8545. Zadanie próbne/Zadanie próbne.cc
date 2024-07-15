58628373e42e54a5092613cb74d973e6fc8379ee
lian2945 <167195801+lian2945@users.noreply.github.com>
2024-07-16 08:39:45 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    string str;
+    cin >> str;
+    for(int i=str.size()-1;i>=0;i--)
+    {
+        cout << str[i];
+    }
+    return 0;
+}
