5202cff72fc27d6f96a55b4a3f0cac50bcb0638e
lian08 <167195801+lian2945@users.noreply.github.com>
2024-09-13 15:13:58 +0900

    [Bronze V] Title: 라면 공식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c;
+    cin >> n;
+    for(int i=0;i<n;i++) {
+        cin >> a >> b >> c;
+        cout << a*(c-1)+b << "\n";
+    }
+    return 0;
+}
