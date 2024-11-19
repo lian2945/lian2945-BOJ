a0a505422756fdce1771ebe51ebb965ac1b8bd90
lian08 <167195801+lian2945@users.noreply.github.com>
2024-11-19 22:38:06 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a*6 + b*3 + c*2 + d + e*2 << "\n";
+    cin >> a >> b >> c >> d >> e;
+    cout << a*6 + b*3 + c*2 + d + e*2;
+    return 0;
+}
