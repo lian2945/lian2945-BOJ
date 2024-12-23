3a36d70b199497864693a5cba63ba11050a53a95
lian08 <167195801+lian2945@users.noreply.github.com>
2024-12-23 22:44:30 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while(n>=5)
+    {
+        cout << "V";
+        n-=5;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout << "I";
+    }
+    return 0;
+}
