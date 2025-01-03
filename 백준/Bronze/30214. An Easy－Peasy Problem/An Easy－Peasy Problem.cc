0682f2af66510b481a3bb27413df4f7d80dff1e3
lian08 <167195801+lian2945@users.noreply.github.com>
2025-01-03 22:56:42 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    if(n*2>=m)
+    {
+        cout << "E";
+    }
+    else
+    {
+        cout << "H";
+    }
+    return 0;
+}
