22eb3d72ec41316b7edbca65084e1f1b5242a362
lian08 <167195801+lian2945@users.noreply.github.com>
2025-02-16 21:19:04 +0900

    [Bronze IV] Title: 인간은 무엇인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32651. 인간은 무엇인가/인간은 무엇인가.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if(n%2024 == 0 && n <= 100000) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
