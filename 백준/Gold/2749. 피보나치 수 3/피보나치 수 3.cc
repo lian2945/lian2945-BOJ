5424639d5171d29627be0a8a343c3a94f15a3757
Lian08 <167195801+lian2945@users.noreply.github.com>
2025-03-02 21:49:38 +0900

    [Gold II] Title: 피보나치 수 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct Matrix {
+    long long matrix[2][2];
+    
+    Matrix operator*(const Matrix& m) const {
+        Matrix result;
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                result.matrix[i][j] = 0;
+                for(int k = 0; k < 2; k++)
+                {
+                    result.matrix[i][j] += matrix[i][k] * m.matrix[k][j];
+                    result.matrix[i][j] %= 1000000;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+Matrix powerMatrix(Matrix base, long long level) {
+    Matrix result = {1, 0, 0, 1};
+    while (level > 0) {
+        if (level % 2 == 1) result = result * base;
+        base = base * base;
+        level /= 2;
+    }
+    return result;
+}
+
+long long fibonacci(long long n) {
+    if (n == 0) return 0;
+    Matrix base = {1, 1, 1, 0};
+    Matrix result = powerMatrix(base, n - 1);
+    return result.matrix[0][0];
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
