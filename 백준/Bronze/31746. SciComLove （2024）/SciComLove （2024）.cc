059e8e29e5ca64ef779c2ff3ca0f885e0f3d5d50
Lian08 <167195801+lian2945@users.noreply.github.com>
2025-03-23 21:36:16 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if(n%2==0) {
+        cout << "SciComLove";
+    }
+    else {
+        cout << "evoLmoCicS";
+    }
+    return 0;
+}
