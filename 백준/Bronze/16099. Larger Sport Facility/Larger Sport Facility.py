57ec3406a20cac25aa5ee601d8d7a64eaca023fd
Lian08 <lian080408@gmail.com>
2025-08-23 23:38:05 +0900

    [Bronze IV] Title: Larger Sport Facility, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/16099. Larger Sport Facility/Larger Sport Facility.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    lt, wt, le, we = map(int, input().split())
+    area_t = lt * wt
+    area_e = le * we
+    if area_t > area_e:
+        print("TelecomParisTech")
+    elif area_e > area_t:
+        print("Eurecom")
+    else:
+        print("Tie")
