21242a5b2f7eabcf21c7bbf35abfea9aa92c5dbe
Lian08 <lian080408@gmail.com>
2025-10-06 23:36:21 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a==b+c || b==a+c || c==a+b) cout << 1;
+    else if(a==b*c || b==a*c || c==a*b) cout << 2;
+    else cout << 3;
+    return 0;
+}
